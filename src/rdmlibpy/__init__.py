c26381aedd33469d36398947963b5eab4722e5d2
thaeber <40823270+thaeber@users.noreply.github.com>
2024-11-01 17:54:19 +0100

    feat: File cache for xarray objects (DataArray & Dataset) (#21)

--- src/rdmlibpy/__init__.py
+++ src/rdmlibpy/__init__.py
@@ -2,6 +2,7 @@
 __version__ = '0.1.12'
 
 import pint_pandas
+import pint
 
 from . import dataframes, loaders, metadata, serializers
 from .process import DelegatedSource
@@ -9,7 +10,7 @@ from .registry import register
 from .workflow import Workflow, run
 
 # set default (short) format for saving/loading units
-pint_pandas.PintType.ureg.default_format = "P~"
+# pint.application_registry.get().formatter.default_format = "P~"
 
 __all__ = [
     dataframes,
