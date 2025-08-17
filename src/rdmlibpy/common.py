8a557e9010b288add0cda8003caa21915fd7d667
thaeber <40823270+thaeber@users.noreply.github.com>
2025-08-17 18:48:08 +0200

    feat: Include data from another YAML file into the workflow

--- src/rdmlibpy/common.py
+++ src/rdmlibpy/common.py
@@ -1,7 +1,11 @@
-from typing import Any
+from pathlib import Path
+from typing import Any, Optional
+
+from omegaconf import OmegaConf
 
 from .base import ProcessBase
 from .registry import register
+from .workflow import Workflow
 
 
 class ScalarSource(ProcessBase):
@@ -16,4 +20,20 @@ class ScalarSource(ProcessBase):
             return self.value
 
 
+class IncludeMetadataFile(ProcessBase):
+    name: str = 'include.metadata.file'
+    version: str = '1'
+
+    def run(self, source: str | Path, key: str | None = None):
+        source = Path(source)
+        conf = OmegaConf.load(source)
+        OmegaConf.resolve(conf)
+
+        if key is not None:
+            conf = OmegaConf.select(conf, key)
+            # conf = cast(DictConfig, conf)
+        return Workflow.create(conf).run()
+
+
 register(ScalarSource())
+register(IncludeMetadataFile())
